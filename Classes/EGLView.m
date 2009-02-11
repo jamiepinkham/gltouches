c9fe877c61234fa38953014b49785ea08f32e161
jamie <jamie@208a7e66-6d34-4273-bd12-10793e77ea72>
2009-02-11 17:34:17 +0000

    cleaned up.. removed extraneous method

--- Classes/EGLView.m
+++ Classes/EGLView.m
@@ -242,7 +242,7 @@
 	[context presentRenderbuffer:GL_RENDERBUFFER_OES];
 	
 }
--(int)nearestPowerOfTwo:(unsigned int)n{
+-(int)nearestPowerOfTwo:(int)n{
     int size = 0;
     
     while (n){
@@ -251,20 +251,6 @@
     }
     return 2 << ( size ? (size-1) : 0 );
 	
-	// that looks like it would work but this is what I use for 4 bytes
-	/*
-	n |= (n >> 1);
-	n |= (n >> 2);
-	n |= (n >> 4);
-	n |= (n >> 8);
-	n |= (n >> 16);
-	return (n+1);
-	*/
-	
-}
-
--(bool)isPowerOfTwo:(size_t)n{
-	return (0 == (n & (n-1)));
 }
 
 -(void)setCubeTexture:(UIImage *)image{
@@ -277,13 +263,16 @@
 		size_t newTextureWidth = [self nearestPowerOfTwo:(int)width];
 		size_t newTextureHeight = [self nearestPowerOfTwo:(int)height];
         
-		UIImage* temp = [self scaleAndRotateImage:image withWidth:(CGFloat)newTextureWidth withHeight:(CGFloat)newTextureHeight];
-        CGImageRef newTextureImage = temp.CGImage;
 		GLubyte* textureData = (GLubyte *) malloc(newTextureWidth * newTextureHeight * 4);
-        CGContextRef textureContext = CGBitmapContextCreate(textureData, newTextureWidth, newTextureHeight, 8, newTextureWidth * 4, CGImageGetColorSpace(newTextureImage), kCGImageAlphaPremultipliedLast);
+        CGContextRef textureContext = CGBitmapContextCreate(textureData, 
+                                                            newTextureWidth, 
+                                                            newTextureHeight, 8, 
+                                                            newTextureWidth * 4, 
+                                                            CGImageGetColorSpace(textureImage), 
+                                                            kCGImageAlphaPremultipliedLast);
         
 		
-		CGContextDrawImage(textureContext, CGRectMake(0,0,(CGFloat)newTextureWidth,(CGFloat)newTextureHeight), newTextureImage);
+		CGContextDrawImage(textureContext, CGRectMake(0,0,(CGFloat)newTextureWidth,(CGFloat)newTextureHeight), textureImage);
         
         CGContextRelease(textureContext);
         
@@ -299,118 +288,8 @@
 		
 		// Set the texture parameters to use a minifying filter and a linear filer (weighted average)
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		
-		// Enable use of the texture
-		// Set a blending function to use
-		//glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-		// Enable blending
-		//glEnable(GL_BLEND);
         
     }
-}
-
-- (UIImage *)scaleAndRotateImage:(UIImage *)image withWidth:(CGFloat)newWidth withHeight:(CGFloat)newHeight{
-    
-    //int kMaxResolution = 570; // Or whatever
-    CGImageRef imgRef = image.CGImage;    
-    CGFloat width = CGImageGetWidth(imgRef);
-    CGFloat height = CGImageGetHeight(imgRef);
-    CGAffineTransform transform = CGAffineTransformIdentity;
-    CGRect bounds = CGRectMake(0, 0, width, height);
-    
-    /*if (width > kMaxResolution || height > kMaxResolution) {
-        CGFloat ratio = width/height;
-        if (ratio > 1) {
-            bounds.size.width = kMaxResolution;
-            bounds.size.height = bounds.size.width / ratio;
-        }
-        else {
-            bounds.size.height = kMaxResolution;
-            bounds.size.width = bounds.size.height * ratio;
-        }
-    }*/
-    
-    CGFloat scaleRatio = bounds.size.width / width;
-    CGSize imageSize = CGSizeMake(CGImageGetWidth(imgRef),
-                                  CGImageGetHeight(imgRef));
-    
-    CGFloat boundHeight;
-    
-    UIImageOrientation orient = image.imageOrientation;
-    switch(orient) {
-        case UIImageOrientationUp: //EXIF = 1
-            transform = CGAffineTransformIdentity;
-            break;
-        case UIImageOrientationUpMirrored: //EXIF = 2
-            transform = CGAffineTransformMakeTranslation(imageSize.width, 0.0);
-            transform = CGAffineTransformScale(transform, -1.0, 1.0);
-            break;
-        case UIImageOrientationDown: //EXIF = 3
-            transform = CGAffineTransformMakeTranslation(imageSize.width, imageSize.
-                                                         height);
-            transform = CGAffineTransformRotate(transform, M_PI);
-            break;
-        case UIImageOrientationDownMirrored: //EXIF = 4
-            transform = CGAffineTransformMakeTranslation(0.0, imageSize.height);
-            transform = CGAffineTransformScale(transform, 1.0, -1.0);
-            break;
-        case UIImageOrientationLeftMirrored: //EXIF = 5
-            boundHeight = bounds.size.height;
-            bounds.size.height = bounds.size.width;
-            bounds.size.width = boundHeight;
-            transform = CGAffineTransformMakeTranslation(imageSize.height, imageSize.
-                                                         width);
-            transform = CGAffineTransformScale(transform, -1.0, 1.0);
-            transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
-            break;
-        case UIImageOrientationLeft: //EXIF = 6
-            boundHeight = bounds.size.height;
-            bounds.size.height = bounds.size.width;
-            bounds.size.width = boundHeight;
-            transform = CGAffineTransformMakeTranslation(0.0, imageSize.width);
-            transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
-            break;
-            
-        case UIImageOrientationRightMirrored: //EXIF = 7
-            boundHeight = bounds.size.height;
-            bounds.size.height = bounds.size.width;
-            bounds.size.width = boundHeight;
-            transform = CGAffineTransformMakeScale(-1.0, 1.0);
-            transform = CGAffineTransformRotate(transform, M_PI / 2.0);
-            break;
-        case UIImageOrientationRight: //EXIF = 8
-            boundHeight = bounds.size.height;
-            bounds.size.height = bounds.size.width;
-            bounds.size.width = boundHeight;
-            transform = CGAffineTransformMakeTranslation(imageSize.height, 0.0);
-            transform = CGAffineTransformRotate(transform, M_PI / 2.0);
-            break;
-        default:
-            break;
-            
-    }
-    
-    UIGraphicsBeginImageContext(bounds.size);
-    CGContextRef contextRef = UIGraphicsGetCurrentContext();
-    if (orient == UIImageOrientationRight || orient == UIImageOrientationLeft)
-    {
-        CGContextScaleCTM(contextRef, -scaleRatio, scaleRatio);
-        CGContextTranslateCTM(contextRef, -height, 0);
-    }
-    else {
-        CGContextScaleCTM(contextRef, scaleRatio, -scaleRatio);
-        CGContextTranslateCTM(contextRef, 0, -height);
-    }
-    
-    CGContextConcatCTM(contextRef, transform);
-    CGContextDrawImage(UIGraphicsGetCurrentContext(), CGRectMake(0, 0, width,
-                                                                 height), imgRef);
-    
-    UIImage *imageCopy = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return imageCopy;
-    
 }
 
 - (void)dealloc {
