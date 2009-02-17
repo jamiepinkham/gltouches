c6a31bc1f6947be0a3c5f4cea37f34d8dfa5512a
Jamie Pinkham <jamie.pinkham@gmail.com>
2009-02-17 15:36:09 -0500

    look at me go!`

--- Classes/GLTouchesAppDelegate.m
+++ Classes/GLTouchesAppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {    
-    
+    //comment to make this need to be commited to git.
     [application setStatusBarHidden:YES animated:YES];
     // Override point for customization after app launch    
     [window addSubview:viewController.view];
